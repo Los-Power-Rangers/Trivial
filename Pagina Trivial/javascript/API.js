ab53a364f0b2dd141a5dc958463411ed73215715
IronFalconer <82568775+IronFalconer@users.noreply.github.com>
2021-04-26 13:20:08 +0200

    Add files via upload

---
+++ Pagina Trivial/javascript/API.js
@@ -0,0 +1,11 @@
+function doGet(event){
+    return responder(event);
+}
+function doPost(event){
+    return responder(event);
+}
+function respoder(event){
+    const {parameter:{nPreg,nombre}} = event
+    nPreg = 0
+    nombre =
+}
